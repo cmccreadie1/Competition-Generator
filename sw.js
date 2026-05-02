60428033c578688e1cafc8ad066500f93dc509ed
Sea Catch Scorer <50778220+cmccreadie1@users.noreply.github.com>
2026-05-02 15:44:25 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,9 @@
-const CACHE_NAME = 'matchpro-v7.7.0-dual-peg-patch';
+const CACHE_NAME = 'matchpro-v7.8.0-compact-slate';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
     './manifest.json',
     './icon.svg',
-    './COMPETITION_TEMPLATE.xlsx',
     'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js',
     'https://cdnjs.cloudflare.com/ajax/libs/exceljs/4.3.0/exceljs.min.js'
 ];
