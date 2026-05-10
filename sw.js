28b2140e1ae5b748007a669d376b5791a8a5c8c0
Sea Catch Scorer <50778220+cmccreadie1@users.noreply.github.com>
2026-05-10 12:50:40 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'zonedraw-v10.49.0-adaptive-mobility-v1';
+const CACHE_NAME = 'zonedraw-v10.50.0-ultimate-integrity-v1';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
