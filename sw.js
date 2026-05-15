6f8ef5d19001bfb75a98bb1def5d7b55f9011289
Sea Catch Scorer <50778220+cmccreadie1@users.noreply.github.com>
2026-05-15 17:29:44 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,6 @@
-const CACHE_NAME = 'zonedraw-v12.04.17-nuclear-backup-v1';
-const ASSETS_TO_CACHE = [
-    './',
-    './index.html',
-    './manifest.json',
-    'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js'
-];
+const CACHE_NAME = 'zonedraw-v12.04.18';
+const ASSETS = ['./', './index.html', './manifest.json', 'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js'];
 
-self.addEventListener('install', (event) => {
-    event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE)));
-    self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-    event.waitUntil(caches.keys().then((names) => Promise.all(names.map((n) => { 
-        if(n !== CACHE_NAME) return caches.delete(n); 
-    }))));
-    self.clients.claim();
-});
-
-self.addEventListener('fetch', (event) => {
-    event.respondWith(caches.match(event.request).then((res) => res || fetch(event.request)));
-});
+self.addEventListener('install', (e) => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))));
+self.addEventListener('activate', (e) => e.waitUntil(caches.keys().then(kn => Promise.all(kn.map(k => { if(k!==CACHE_NAME) return caches.delete(k); })))));
+self.addEventListener('fetch', (e) => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
